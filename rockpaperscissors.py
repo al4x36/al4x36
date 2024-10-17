07fd77cbc32073f52c915586e46a6513cc3cf38c
al4x36 <agaffney0715@gmail.com>
2024-10-17 09:04:47 -0400

    Update and rename roPaScLiSp.py to rockpaperscissors.py
    Rock, Paper, Scissors, lizard, Spock!
     Author: Alaxavier Gaffney <agaffney2718031@woonsocketschools.com>

---
+++ rockpaperscissors.py
@@ -0,0 +1,38 @@
+# Rock, Paper, Scissors, lizard, Spock!
+# Author: Alaxavier Gaffney <agaffney2718031@woonsocketschools.com>
+
+
+import random
+score1 = score2 = 0
+isPlaying = True
+
+while isPlaying is True:
+    print("Let's play Rock, Paper, Scissors, Lizard, Spock!")
+choices = ["rock", "paper", "scissors", "spock", "lizard"]
+userChoice = input("Choose rock, paper, spock, lizard, or scissors: ").lower()
+computerChoice = random.choice(choices)
+    if userChoice in choices:
+        if userChoice == computerChoice:
+            print("It's a tie!")
+        elif (
+    
+                (userChoice == "rock" and computerChoice == "scissors")
+            or (userChoice == "paper" and computerChoice == "rock")
+            or (userChoice == "scissors" and computerChoice == "paper")
+            or (userChoice == "lizard" and computerChoice == "spock")
+            or (userChoice == "rock" and computerChoice == "lizard")
+            or (userChoice == "scissors" and computerChoice == "lizard")
+            or (userChoice == "spock" and computerChoice == "scissors")
+         ):
+
+            print("You win! fatality! flawless victory! You chose: ", userChoice,"and computer chose: ", computerChoice)
+        
+    else:
+            print("Computer wins! flawless victory! Computer chose: ", computerChoice, "and you chose: ", userChoice)
+        
+    ask = input("Do you want to play again? (yes/no): ").lower()
+    userChoice = input("Choose rock, paper, spock, lizard, or scissors: ").lower()
+    computerChoice = random.choice(choices)
+    if ask != "yes":
+        isPlaying = False
+
